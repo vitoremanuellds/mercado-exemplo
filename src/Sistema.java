3a59834526d1a57336f9756d70a6885975ea2d93
Vitor Emanuel <vemanuel.wt@gmail.com>
2022-05-16 08:45:36 -0300

    Creating some tests in the main for the new method listProduto

--- src/Sistema.java
+++ src/Sistema.java
@@ -6,21 +6,32 @@ public class Sistema {
 
 	public static void main(String[] args) {
 		
-		Produto produtoLeite = new Produto("Leite", "Parmalat");
+		Produto produtoLeite = new Produto("Leite em pó", "Parmalat");
 		Lote lote = new Lote(produtoLeite, 10L);
 		ProdutoRepository catalogo = new ProdutoRepository();
 
 		Produto produtoPao = new Produto("Pao", "Francês");
-		Produto produtoOvo = new Produto("Ovo", "Galinha");
+		Produto produtoLeiteCaixa = new Produto("Leite de caixa", "Piracanjuba");
+		Produto produtoSuco = new Produto("Suco", "Árvore");
+		Produto produtoOvoCodorna = new Produto("Ovo de Codorna", "Codorna");
+		Produto produtoPaoDoce = new Produto("Pão Doce", "Padaria");
 
 		catalogo.addProduto(produtoLeite);
 		catalogo.addProduto(produtoPao);
-		catalogo.addProduto(produtoOvo);
+		catalogo.addProduto(produtoOvoCodorna);
+		catalogo.addProduto(produtoPaoDoce);
+		catalogo.addProduto(produtoLeiteCaixa);
+		catalogo.addProduto(produtoSuco);
+
 
 		System.out.println(catalogo);
 		System.out.println(lote);
 
-		catalogo.removeProduto(produtoLeite.getId());
+		// catalogo.removeProduto(produtoLeite.getId());
+
+		for (Produto produto : catalogo.listProduto("Leite")) {
+			System.out.println(produto);
+		}
 
 		System.out.println(catalogo);
 	}
